d88761342d33d022c7e9e22812f240464a7a028f
liyongxiang <liyongxiang@hoolai.com>
2017-03-17 10:17:10 +0800

    自定义菊花ProgressDialog样式

--- app/src/main/java/com/android/hoolai/pack/views/MyProgressDialog.java
+++ app/src/main/java/com/android/hoolai/pack/views/MyProgressDialog.java
@@ -10,7 +10,8 @@ import com.android.hoolai.pack.R;
 public class MyProgressDialog extends Dialog {
 
     public MyProgressDialog(@NonNull Context context) {
-        super(context);
+//        super(context);
+        super(context, R.style.progress_dialog);
         setContentView(R.layout.custom_my_progress_dialog);
         setCancelable(true);
         getWindow().setBackgroundDrawableResource(android.R.color.transparent);
